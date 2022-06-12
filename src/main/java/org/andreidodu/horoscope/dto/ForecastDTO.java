04f7cad7d625de3f4e2c28d1ce76ab4a89f616c2
andrei <andrei@europa>
2022-06-12 08:03:51 +0200

    new dto

---
+++ src/main/java/org/andreidodu/horoscope/dto/ForecastDTO.java
@@ -0,0 +1,13 @@
+package org.andreidodu.horoscope.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ForecastDTO {
+
+	private String sign;
+	private String forecast;
+	
+}
